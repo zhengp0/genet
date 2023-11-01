142b16e087db3b5e82032c5e9b253a763bbd0101
Peng Zheng <zhengp@uw.edu>
2023-11-01 10:11:37 +0800

    add the test for yue's simple case

---
+++ tests/test_for_yue.py
@@ -0,0 +1,31 @@
+import numpy as np
+from genet.solver import RegRelSolver
+
+
+def test_on_simple_case():
+    n = 5
+    k0 = np.identity(n)
+
+    at = np.zeros((n, n))
+    at[0, 1] = 0.1
+    exp_at = np.identity(n) + at
+
+    kt = exp_at.dot(k0).dot(exp_at.T)
+
+    inv_k0 = np.linalg.inv(k0)
+    inv_kt = np.linalg.inv(kt)
+
+    solver = RegRelSolver(inv_k0, inv_kt)
+    my_at = solver.fit()
+    my_exp_at = np.identity(n) + my_at
+
+    # forward
+    fresidual = kt - my_exp_at.dot(k0).dot(my_exp_at.T)
+    # backward
+    bresidual = my_exp_at.T.dot(inv_kt).dot(my_exp_at) - inv_k0
+
+    assert np.allclose(fresidual, 0.0)
+    assert np.allclose(bresidual, 0.0)
+
+    # however we cannot gaurantee exp_at is close to my_exp_at
+    # assert np.allclose(my_exp_at, exp_at) <- this will fail the test
