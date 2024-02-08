e27f9e65bba3471d3bb986a913df9cb0990d40d9
zhengp0 <zhengp@uw.edu>
2024-02-08 09:30:57 -0800

    update the formula for the forward model

--- tests/test_for_yue.py
+++ tests/test_for_yue.py
@@ -10,7 +10,7 @@ def test_on_simple_case():
     at[0, 1] = 0.1
     exp_at = np.identity(n) + at
 
-    kt = exp_at.dot(k0).dot(exp_at.T)
+    kt = exp_at.T.dot(k0).dot(exp_at)
 
     inv_k0 = np.linalg.inv(k0)
     inv_kt = np.linalg.inv(kt)
@@ -20,9 +20,9 @@ def test_on_simple_case():
     my_exp_at = np.identity(n) + my_at
 
     # forward
-    fresidual = kt - my_exp_at.dot(k0).dot(my_exp_at.T)
+    fresidual = kt - my_exp_at.T.dot(k0).dot(my_exp_at)
     # backward
-    bresidual = my_exp_at.T.dot(inv_kt).dot(my_exp_at) - inv_k0
+    bresidual = my_exp_at.dot(inv_kt).dot(my_exp_at.T) - inv_k0
 
     assert np.allclose(fresidual, 0.0, atol=1e-7)
     assert np.allclose(bresidual, 0.0, atol=1e-7)
